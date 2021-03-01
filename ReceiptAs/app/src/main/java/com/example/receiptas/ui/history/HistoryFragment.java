8d58d15a5907e8f21061963c748f88b1ecaceac2
Orel <aurelien.dellac@gmail.com>
2021-02-28 20:00:11 -0500

    Recycler view d'autres modifs

--- ReceiptAs/app/src/main/java/com/example/receiptas/ui/history/HistoryFragment.java
+++ ReceiptAs/app/src/main/java/com/example/receiptas/ui/history/HistoryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,25 +17,36 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.receiptas.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HistoryFragment extends Fragment {
 
     private HistoryViewModel historyViewModel;
     private RecyclerView historyRecyclerView;
+    private ReceiptAdapter adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         historyViewModel =
                 new ViewModelProvider(this).get(HistoryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_history, container, false);
-
+        historyViewModel.getReceipts().observe(getViewLifecycleOwner(), receiptListUpdateObserver);
         historyRecyclerView = root.findViewById(R.id.history_recycler_view);
         this.configureRecyclerView();
         return root;
     }
 
     private void configureRecyclerView() {
-        //adapter = new ReceiptAdapter(historyViewModel.getReceipts().getValue());
-
+        adapter = new ReceiptAdapter(new ArrayList<String>());
+        historyRecyclerView.setAdapter(adapter);
         historyRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
+
+    private Observer<List<String>> receiptListUpdateObserver = new Observer<List<String>>() {
+        @Override
+        public void onChanged(List<String> strings) {
+            adapter = new ReceiptAdapter(strings);
+        }
+    };
 }
