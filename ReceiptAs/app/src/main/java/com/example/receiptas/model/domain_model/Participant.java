7b71c0e16b64ad203509d8b4c129dce24d3b0fe5
Orel <aurelien.dellac@gmail.com>
2021-03-17 16:41:24 -0400

    Setup domain model (avec les changements cette fois)

--- ReceiptAs/app/src/main/java/com/example/receiptas/model/domain_model/Participant.java
+++ ReceiptAs/app/src/main/java/com/example/receiptas/model/domain_model/Participant.java
@@ -1,4 +1,28 @@
 package com.example.receiptas.model.domain_model;
 
+import javax.inject.Inject;
+
 public class Participant {
+    private int id;
+    private String name;
+    private boolean payer;
+
+    @Inject
+    public Participant(int id, String name, boolean payer) {
+        this.id = id;
+        this.name = name;
+        this.payer = payer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isPayer() {
+        return payer;
+    }
 }
