13cd6198f8c515c8ce064132a4404e476e29f35f
Orel <aurelien.dellac@gmail.com>
2021-03-19 21:12:02 -0400

    display items in correction view + loading screen

--- ReceiptAs/app/src/main/java/com/example/receiptas/model/util/DataState.java
+++ ReceiptAs/app/src/main/java/com/example/receiptas/model/util/DataState.java
@@ -1,32 +1,36 @@
 package com.example.receiptas.model.util;
 
+import androidx.lifecycle.MutableLiveData;
+
 public class DataState<T> {
     private T data;
     private Throwable error;
-    private State state;
+    private final MutableLiveData<State> state = new MutableLiveData<>();
 
     public T getData() {
         return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
     }
 
     public Throwable getError() {
         return error;
     }
 
+    public MutableLiveData<State> getState() {
+        return this.state;
+    }
+
+    public void setSuccess(T data) {
+        this.data = data;
+        this.state.setValue(State.SUCCESS);
+    }
+
     public void setError(Throwable error) {
         this.error = error;
+        this.state.setValue(State.ERROR);
     }
 
-    public State getState() {
-        return state;
-    }
-
-    public void setState(State state) {
-        this.state = state;
+    public void setLoading() {
+        this.state.setValue(State.LOADING);
     }
 
     public enum State {
--- ReceiptAs/app/src/main/java/com/example/receiptas/ui/scan_receipt/correction/ItemCorrectionFragment.java
+++ ReceiptAs/app/src/main/java/com/example/receiptas/ui/scan_receipt/correction/ItemCorrectionFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,36 +14,32 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.receiptas.R;
+import com.example.receiptas.model.util.DataState;
 import com.example.receiptas.ui.history.HistoryFragmentDirections;
 import com.example.receiptas.ui.history.OnRecyclerViewItemClickListener;
 import com.example.receiptas.ui.history.ReceiptAdapter;
+import com.example.receiptas.ui.scan_receipt.ScanReceiptViewModel;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ItemCorrectionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class ItemCorrectionFragment extends Fragment {
+
+    private ScanReceiptViewModel scanReceiptViewModel;
     private RecyclerView itemRecyclerView;
     private ItemAdapter adapter;
-
-    public ItemCorrectionFragment() {
-        // Required empty public constructor
-    }
-
-    public static ItemCorrectionFragment newInstance() {
-        ItemCorrectionFragment fragment = new ItemCorrectionFragment();
-        return fragment;
-    }
+    private ProgressBar progressBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        scanReceiptViewModel = new ViewModelProvider(getActivity()).get(ScanReceiptViewModel.class);
     }
 
     @Override
@@ -54,6 +52,8 @@ public class ItemCorrectionFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        this.progressBar = view.findViewById(R.id.progressBar);
+
         TextView informationMessage = view.findViewById(R.id.information_message);
         informationMessage.setText(getResources().getString(R.string.information_message_item_correction));
 
@@ -62,7 +62,13 @@ public class ItemCorrectionFragment extends Fragment {
     }
 
     private void configureRecyclerView() {
-        this.adapter = new ItemAdapter(new ArrayList<>(), onItemOptionSelected);
+        if(this.scanReceiptViewModel.getItems().getValue().getState().getValue() == DataState.State.SUCCESS) {
+            System.out.println("ALREADY LOADED");
+            this.adapter = new ItemAdapter(this.scanReceiptViewModel.getItems().getValue().getData(), onItemOptionSelected);
+        } else {
+            this.adapter = new ItemAdapter(new ArrayList<>(), onItemOptionSelected);
+            this.scanReceiptViewModel.getItems().getValue().getState().observe(getViewLifecycleOwner(), itemsObserver);
+        }
         this.itemRecyclerView.setAdapter(adapter);
         this.itemRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
@@ -70,4 +76,31 @@ public class ItemCorrectionFragment extends Fragment {
     private final OnRecyclerViewItemClickListener onItemOptionSelected  = item -> {
         //TODO PROCESS ITEM MENU ACTION
     };
+
+    private final Observer<DataState.State> itemsObserver = new Observer<DataState.State>() {
+        @Override
+        public void onChanged(DataState.State dataState) {
+            switch(dataState) {
+                case SUCCESS:
+                    displayProgressBar(false);
+                    itemRecyclerView.setAdapter(
+                        new ItemAdapter(scanReceiptViewModel.getItems().getValue().getData(), onItemOptionSelected)
+                    );
+                    break;
+                case ERROR:
+                    displayProgressBar(false);
+                    scanReceiptViewModel.getItems().getValue().getError().printStackTrace();
+                    break;
+                case LOADING:
+                    displayProgressBar(true);
+                    break;
+                default:
+                    //do nothing
+            }
+        }
+    };
+
+    private void displayProgressBar(boolean isDisplayed) {
+        this.progressBar.setVisibility(isDisplayed ? View.VISIBLE : View.GONE);
+    }
 }
