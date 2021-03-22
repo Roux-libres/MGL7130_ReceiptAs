ef9a2da8e1efc086a1f6bd76061705ef478fe5fd
Nelson DEREN <nelson.deren@viacesi.fr>
2021-03-23 00:55:50 +0100

    Finishing articles_division + fix adding participant panel + modification toolbar

--- ReceiptAs/app/src/main/java/com/example/receiptas/ui/division/AddingParticipantsFragment.java
+++ ReceiptAs/app/src/main/java/com/example/receiptas/ui/division/AddingParticipantsFragment.java
@@ -1,10 +1,7 @@
 package com.example.receiptas.ui.division;
 
-import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.content.res.ColorStateList;
-import android.content.res.TypedArray;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,35 +10,43 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.receiptas.R;
+import com.example.receiptas.model.domain_model.Participant;
 import com.example.receiptas.ui.scan_receipt.ScanReceiptViewModel;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class AddingParticipantsFragment extends Fragment {
 
     private ScanReceiptViewModel scanReceiptViewModel;
     private GridView gridNames;
-    private NamesAdapter namesAdapter;
+    private ParticipantAdapter participantAdapter;
 
     public static AddingParticipantsFragment newInstance() {
         return new AddingParticipantsFragment();
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setHasOptionsMenu(true);
         scanReceiptViewModel = new ViewModelProvider(getActivity()).get(ScanReceiptViewModel.class);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.validate, menu);
     }
 
     @Override
@@ -53,9 +58,12 @@ public class AddingParticipantsFragment extends Fragment {
         information_message.setText(R.string.information_message_adding_participants);
 
         this.gridNames = root.findViewById(R.id.names_grid);
-        this.namesAdapter = new NamesAdapter(this.getContext(), null);
+        ArrayList<String> names = new ArrayList<>();
+        for(Participant participant: this.scanReceiptViewModel.getTheReceipt().getParticipants())
+            names.add(participant.getName());
+        this.participantAdapter = new ParticipantAdapter(this.getContext(), this.scanReceiptViewModel.getTheReceipt());
 
-        gridNames.setAdapter(namesAdapter);
+        gridNames.setAdapter(participantAdapter);
         gridNames.setVerticalScrollBarEnabled(false);
         gridNames.setEnabled(false);
 
@@ -65,25 +73,28 @@ public class AddingParticipantsFragment extends Fragment {
             public void onClick(View v) {
                 TextInputEditText editName = (TextInputEditText) root.findViewById(R.id.name_input_text);
                 String inputText = editName.getText().toString();
-                if (inputText.trim().length() > 0 && namesAdapter.addName(inputText)) {
+                if (inputText.trim().length() > 0 && scanReceiptViewModel.getTheReceipt().getParticipants().size() < 8) {
+                    scanReceiptViewModel.getTheReceipt().addParticipantByName(inputText);
                     editName.getText().clear();
-                    namesAdapter.notifyDataSetChanged();
+                    participantAdapter.notifyDataSetChanged();
                 }
             }
         });
 
-        FloatingActionButton validation = root.findViewById(R.id.validate_button);
-        validation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(saveParticipants()) {
-                    AddingParticipantsFragmentDirections.ActionAddingParticipantsFragmentToItemDivision action =
-                            AddingParticipantsFragmentDirections.actionAddingParticipantsFragmentToItemDivision();
-                    Navigation.findNavController(view).navigate(action);
-                }
-            } });
-
         return root;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.validate_button) {
+            AddingParticipantsFragmentDirections.ActionAddingParticipantsFragmentToItemDivision action =
+                    AddingParticipantsFragmentDirections.actionAddingParticipantsFragmentToItemDivision();
+            Navigation.findNavController(getView()).navigate(action);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+
+        }
     }
 
     @Override
@@ -96,14 +107,4 @@ public class AddingParticipantsFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
     }
 
-    private boolean saveParticipants() {
-        ArrayList<String> names = this.namesAdapter.getNames();
-        if(names.size() > 0) {
-            for(String name: names) {
-                scanReceiptViewModel.getTheReceipt().addParticipantByName(name);
-            }
-            return true;
-        }
-        return false;
-    }
 }
